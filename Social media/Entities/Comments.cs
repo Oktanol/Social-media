86aef001d48ea08370a3f9ebbf1fc8fd24fc4505
Oktanol <123610613+Oktanol@users.noreply.github.com>
2023-03-18 16:57:05 +0600

    Update Comments.cs

--- Social media/Entities/Comments.cs
+++ Social media/Entities/Comments.cs
@@ -2,11 +2,13 @@
 {
     public class Comments
     {
-        public int Id { get; set; }
-        public Posts Post { get; set; }
-        public Users User { get; set; }
-        public string text { get; set; }
-        public DateTime Date { get; set; }
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int PostId { get; set; }
+    public string Content { get; set; }
+    public DateTime Date { get; set; }
+    public Users Users { get; set; }
+    public Posts Posts { get; set; }
 
     }
 }
